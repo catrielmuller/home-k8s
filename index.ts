e7f664aa5c79f06f2275f56908aa2cd3aabbbb4e
Catriel Müller <catrielmuller@gmail.com>
2021-10-23 03:07:28 -0300

    Ingress Nginx

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as k8s from "@pulumi/kubernetes";
 import { Config } from './configs'
 import { MetalLBModule } from "./modules/metallb";
 import { NFSSubDirExternalProvisionerModule } from './modules/nfs'
+import { IngressNginxModule } from './modules/ingress-nginx'
 
 const systemNamespace = new k8s.core.v1.Namespace('system-namespace', {
     metadata: {
@@ -11,3 +12,4 @@ const systemNamespace = new k8s.core.v1.Namespace('system-namespace', {
 
 const MetalLB = MetalLBModule(systemNamespace)
 const NFS = NFSSubDirExternalProvisionerModule(systemNamespace)
+const IngressNginx = IngressNginxModule(systemNamespace)
